9366a8c3f4220c9e92f34907cc1861df441387cb
royeyu <1755431269@qq.com>
2018-01-22 22:51:16 +0800

    pom.xml文件更新

---
+++ roye-security-demo/src/main/java/cn/roye/DemoApplication.java
@@ -0,0 +1,17 @@
+package cn.roye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 1. @RestController is a stereotype annotation that combines @ResponseBody and @Controller
+ * 2. 这个类必须要放到一个package里面。
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
