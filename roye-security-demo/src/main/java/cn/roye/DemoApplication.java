5265c4f26a973dd69396408843afaf1a9b63439d
royeyu <1755431269@qq.com>
2018-01-22 23:23:52 +0800

    hello world

--- roye-security-demo/src/main/java/cn/roye/DemoApplication.java
+++ roye-security-demo/src/main/java/cn/roye/DemoApplication.java
@@ -2,6 +2,7 @@ package cn.roye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,5 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "public String hello";
     }
 }
